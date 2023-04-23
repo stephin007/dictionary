fb02bd030e15b88d453eb170739ca07f71b3f86c
Stephin Reji <stephinreji@gmail.com>
2023-04-23 19:18:23 +0530

    set up basic routes and MIUI theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import theme from "./theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//
+import Home from "./components/Home";
+import Bookmarks from "./components/Bookmarks";
+import Definition from "./components/Definition";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/bookmarks' element={<Bookmarks />} />
+          <Route path='/search/:word' element={<Definition />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+  palette: {
+    background: {
+      default: "#F1F3F4",
+    },
+  },
+});
